7cf37e19dc1e3ed5e6f1ae302a32f33814fa669a
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2025-01-06 11:37:49 +0100

    add repos

---
+++ DB/AttributeRepo/Attributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.AttributeRepo
+{
+    public class Attributes
+    {
+        public int Attribute_id { get; set; }
+        public string Attribute_name { get; set; }
+    }
+}
